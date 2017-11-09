c9898a7b4ef2e4115cff6084609ddcd48d964f8d
deividsantos <deividfranciscosantos@hotmail.com>
2017-11-09 16:53:59 -0200

    Command finalizado 2

--- Command/JogadorTesteCommand/src/main/java/Main.java
+++ Command/JogadorTesteCommand/src/main/java/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
         SoccerPlayer soccerPlayer = new SoccerPlayer("Deivid", 1.7, 70);
 
-        KickPlayerCommand kickCommand = new KickPlayerCommand(soccerPlayer);
-        PassPlayerCommand passCommand = new PassPlayerCommand(soccerPlayer);
-        CrossPlayerCommand crossCommand = new CrossPlayerCommand(soccerPlayer);
+        PlayerCommand kickCommand = new KickPlayerCommand(soccerPlayer);
+        PlayerCommand passCommand = new PassPlayerCommand(soccerPlayer);
+        PlayerCommand crossCommand = new CrossPlayerCommand(soccerPlayer);
 
         Broker broker = new Broker();
         broker.takePlayer(kickCommand);
